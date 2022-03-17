35d7ad683edb177a0add2756d3e0a35cb01549bd
Armando Alvear <armando.alvear01@outlook.com>
2022-03-16 20:19:57 -0500

    fix broken image link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "../images/react-logo.png";
 
 export default function Navbar() {
   return (
     <nav>
-      <img src="../images/react-logo.png" />
+      <img src={logo} />
       <h3>Test Nav</h3>
     </nav>
   );
