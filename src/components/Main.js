f7f93e2d24f9a375851c742a07a6745f98ce23ee
Armando Alvear <armando.alvear01@outlook.com>
2022-03-16 20:16:03 -0500

    code components

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <div>
+      <h1>Test Main</h1>
+    </div>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="../images/react-logo.png" />
+      <h3>Test Nav</h3>
+    </nav>
+  );
+}
