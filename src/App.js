faed9f7dd9597614736b9049c8593a99161fa4ec
Armando Alvear <armando.alvear01@outlook.com>
2022-06-12 00:19:17 -0500

    project 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import style from "./style.css";
@@ -7,7 +7,7 @@ import style from "./style.css";
 export default function App() {
   return (
     <div className="container">
-      <Header />
+      <Navbar />
       <Main />
       <Footer />
     </div>
